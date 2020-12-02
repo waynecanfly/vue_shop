0911c3b95c18c63bf7ad0ccac87519d3248e52c0
waynecanfly <1370153124@qq.com>
2020-12-02 15:39:20 +0800

    使用路由导航守卫控制页面权限

--- vue_shop/src/router/index.js
+++ vue_shop/src/router/index.js
@@ -5,10 +5,21 @@ import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
-export default new VueRouter({
+const router = new VueRouter({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
     { path: '/home', component: Home }
   ]
 })
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  if (to.path === 'login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+
+export default router
